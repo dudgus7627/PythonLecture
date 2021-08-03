50e3597da7fa2200e44a1b03a517ec106e6305b1
KimSuHyeon <dudgus7627@gmail.com>
2021-08-03 17:59:24 +0900

    함수연습3

--- Function2.py
+++ Function2.py
@@ -13,3 +13,47 @@ def func2(a):
 
 #호출
 print(func2(1))
+
+#불변형식을 함수 내부에서 읽기+쓰기
+g = 1
+def testScope(a):
+    #global g
+    g = 2
+    return g+a
+
+#호출
+print(testScope(1))
+print("전역변수 g : ", g)
+
+#기본값을 세팅
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(a=5))
+print(times(3,4))
+
+#키워드 인자(파라메터명을 명시)
+def connectURL(server, port):
+    strUAL = "http://" + server + ":" + port
+    return strUAL
+
+#호출
+print(connectURL("bitCamp", "80"))
+print(connectURL(port="80", server="bitCamp"))
+
+#가변인자(다양한 갯수를 소화)
+def union(*ar):
+    #지역변수(리스트)
+    result = []
+    for item in ar:
+    #H(0) | A(1) | M(2)
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+#호출
+print(union("HAN", "EGG"))
+print(union("HAM","EGG", "SPAM"))
